d328fe58328a30acc6610d50131ac0c97723324b
Manuel <mrodbon99@gmail.com>
2022-06-02 14:01:41 +0200

    Wall Jump and Dash!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -31,10 +31,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int nExtraJumps = 1;
     [SerializeField] private float hangTime= 0.1f;
     [SerializeField] private float jumpBufferLen= 0.1f;
+    private bool isJumping;
     private int extraJumpsValue;
     private float hangTimeCounter;
     private float jumpBufferCounter;
-    private bool canJump => jumpBufferCounter > 0.0f && (hangTimeCounter > 0f || extraJumpsValue > 0);
+    private bool canJump => jumpBufferCounter > 0.0f && (hangTimeCounter > 0f || extraJumpsValue > 0 || onWall);
 
     [Header("Ground Collision")]
     [SerializeField] private float groundRaycastLength;
@@ -44,8 +45,19 @@ public class PlayerMovement : MonoBehaviour
     [Header("Wall Jump")]
     [SerializeField] private float wallRayCastLenght;
     [SerializeField] private float wallSlideMod;
+    [SerializeField] private float wallJumpXVel = 0.2f;
     public bool onWall;
     public bool onRightWall;
+
+    [Header("Dash")]
+    [SerializeField] private float dashSpeed = 15f;
+    [SerializeField] private float dashLength = .3f;
+    [SerializeField] private float dashBufferLength = 0.1f;
+    private float dashBufferCounter;
+    private bool hasDashed = false;
+    private bool isDashing;
+    private bool canDash => dashBufferCounter > 0.0f && !hasDashed;
+
 
     [Header ("Corner Correction")]
     [SerializeField] private float topRayCastLen;
@@ -68,33 +80,94 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", Mathf.Abs(horDir));
         if (Input.GetButtonDown("Jump")) jumpBufferCounter = jumpBufferLen;
         else jumpBufferCounter -= Time.deltaTime;
-        if (canJump) Jump();
+        
+        if (Input.GetButtonDown("Dash"))
+        {
+            Debug.Log(Input.GetButtonDown("Dash"));
+            dashBufferCounter = dashBufferLength;
+            Debug.Log(canDash);
+        }
+
+        else dashBufferCounter -= Time.deltaTime;
+
     }
     private void FixedUpdate()
     {
         CheckCollisions();
-        if (canMove) MoveCharacter();
-        if (isGrounded)
+        //Debug.Log(canDash);
+        if (canDash) StartCoroutine(Dash(horDir, verDir));
+        if (!isDashing)
         {
-            animator.SetBool("IsJumping", false);
-            extraJumpsValue = nExtraJumps; 
-            hangTimeCounter = hangTime;
-            ApplyGroundLinearDrag();
+            if (canMove) MoveCharacter();
+            else _rb.velocity = Vector2.Lerp(_rb.velocity, (new Vector2(horDir * maxMovSpeed, _rb.velocity.y)), 0.5f * Time.fixedDeltaTime);
+            if (isGrounded)
+            {
+                animator.SetBool("IsJumping", false);
+                extraJumpsValue = nExtraJumps;
+                hangTimeCounter = hangTime;
+                ApplyGroundLinearDrag();
+                hasDashed = false;
+            }
+            else
+            {
+                animator.SetBool("IsJumping", true);
+                ApplyAirLinearDrag();
+                FallMultiplier();
+                hangTimeCounter -= Time.fixedDeltaTime;
+                if (!onWall || _rb.velocity.y < 0f) isJumping = false;
+            }
+            if (canJump)
+            {
+                if (onWall && !isGrounded)
+                {
+                    WallJump();
+                    Flip();
+                }
+                else
+                {
+                    Jump(Vector2.up);
+                }
+
+            }
+            if (!isJumping)
+            {
+                if (wallGrab) WallGrab();
+                if (wallSlide) WallSlide();
+            }
+
         }
-        else
-        {
-            animator.SetBool("IsJumping", true);
-            ApplyAirLinearDrag();
-            FallMultiplier();
-            hangTimeCounter -= Time.fixedDeltaTime;
-        }
+
         if (canCorner) CornerCorrect(_rb.velocity.y);
-        if (wallGrab) WallGrab();
-        if (wallSlide) WallSlide();
     }
     private static Vector2 GetInput()
     {
         return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    IEnumerator Dash(float x, float y)
+    {
+        float dashStart = Time.time;
+        hasDashed = true;
+        isDashing = true;
+        isJumping = false;
+
+        _rb.velocity = Vector2.zero;
+        _rb.gravityScale = 0f;
+        _rb.drag = 0f;
+
+        Vector2 dir;
+        if (x != 0f || y != 0f) dir = new Vector2(x, y);
+        else
+        {
+            if (facingRight) dir = new Vector2(1, 0);
+            else dir = new Vector2(-1,0);
+        }
+        while (Time.time < dashStart + dashLength)
+        {
+            _rb.velocity = dir.normalized * dashSpeed;
+            yield return null;
+        }
+        isDashing = false;
     }
     private void MoveCharacter()
     {
@@ -106,6 +179,7 @@ public class PlayerMovement : MonoBehaviour
         {
             Flip();
         }
+
         _rb.AddForce(new Vector2(horDir, 0f) * movAcce);
         if (Mathf.Abs(_rb.velocity.x) > maxMovSpeed)
         {
@@ -114,6 +188,7 @@ public class PlayerMovement : MonoBehaviour
     }
     private void WallGrab()
     {
+        hasDashed = false;
         _rb.gravityScale = 0;
         _rb.velocity = new Vector2(_rb.velocity.x, 0);
     }
@@ -136,22 +211,48 @@ public class PlayerMovement : MonoBehaviour
     {
         _rb.drag = airDrag;
     }
-    private void Jump()
+    private void Jump(Vector2 direc)
     {
+        Debug.Log(direc);
         animator.SetBool("IsJumping", true);
-        if (!isGrounded){
-            extraJumpsValue--;
-            _rb.AddForce(Vector2.up * jumpForce*0.8f, ForceMode2D.Impulse);
+        if (direc == Vector2.up)
+        {
+            if (!isGrounded && !onWall)
+            {
+                extraJumpsValue--;
+                _rb.velocity = new Vector2(_rb.velocity.x, 0);
+                _rb.AddForce(direc * jumpForce * 0.8f, ForceMode2D.Impulse);
+            }
+            else
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, 0);
+                _rb.AddForce(direc * jumpForce, ForceMode2D.Impulse);
+            }
         }
         else
         {
-            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if (!isGrounded && !onWall)
+            {
+                extraJumpsValue--;
+                _rb.velocity = new Vector2(_rb.velocity.x, 0);
+                _rb.AddForce(direc * jumpForce * 0.8f, ForceMode2D.Impulse);
+            }
+            else
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, 0);
+                _rb.AddForce(direc * jumpForce * 1.5f, ForceMode2D.Impulse);
+            }
         }
-        //_rb.velocity = new Vector2(_rb.velocity.x, 0);
-        
         hangTimeCounter = 0f;
         jumpBufferCounter = 0f;
+        isJumping = true;
     }
+    private void WallJump()
+    {
+        Vector2 jumpDir = onRightWall ? Vector2.left : Vector2.right;
+        Jump(Vector2.up/2 + jumpDir);
+    }
+    
     private void FallMultiplier()
     {
         if (_rb.velocity.y < 0)  _rb.gravityScale = fallSpeed;
