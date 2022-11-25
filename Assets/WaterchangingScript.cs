4ca3ef751fc51da6d0af68cd26f543e2ea40ced9
Mike-FG <mastersmfg@gmail.com>
2022-11-25 12:35:12 +0100

    4 scenario water

---
+++ Assets/WaterchangingScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterchangingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material ShaderMaterial1;
+    public float elapsedTime = 0;
+
+    void Start()
+    {
+        //ShaderMaterial1 = new Material(Shader.Find("Shader Graphs/try1"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        ShaderMaterial1.SetFloat("_Movement", elapsedTime);
+    }
+}
